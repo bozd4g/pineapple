dbdf35875e473402681d698f5ad8ae395d446b7f
Furkan Bozdag <bozd4g@hotmail.com>
2019-01-22 22:55:18 +0300

    Add prettier package and their .rc file

--- src/scenes/Index/index.tsx
+++ src/scenes/Index/index.tsx
@@ -1,5 +1,9 @@
-import React from "react";
+import React from 'react'
 
-class Index extends React.Component {}
+class Index extends React.Component {
+  render() {
+    return <div />
+  }
+}
 
-export default Index;
+export default Index
