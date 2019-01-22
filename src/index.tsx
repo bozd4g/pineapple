6a703ad412dce86b625f86fca20b6314453d9cc7
Furkan Bozdag <bozd4g@hotmail.com>
2019-01-22 22:37:31 +0300

    Create file structure of project

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,7 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import registerServiceWorker from "./registerServiceWorker";
+import App from "./App";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
