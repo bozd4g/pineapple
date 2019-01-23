572345ee15b7714e52232efd781fc77dc798fd85
Furkan Bozdag <bozd4g@hotmail.com>
2019-01-23 22:59:41 +0300

    Add svg package. Edit index component

--- src/scenes/Index/index.tsx
+++ src/scenes/Index/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Row } from 'react-flexbox-grid';
+import './index.css';
 
 class Index extends React.Component {
   render() {
-    return <div />;
+    return <Row className="main" style={{ background: `linear-gradient(45deg, ${'#A83279, #FFB246'})` }} />;
   }
 }
 
