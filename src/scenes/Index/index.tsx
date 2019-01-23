33f1afb03459ed4a086b7da873ba964f7455b6ce
Furkan Bozdag <bozd4g@hotmail.com>
2019-01-23 23:40:49 +0300

    Add seperator file as svg. Provide main side

--- src/scenes/Index/index.tsx
+++ src/scenes/Index/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Row } from 'react-flexbox-grid';
+import ReactSVG from 'react-svg';
+import indexSVG from '../../images/index.svg';
 import './index.css';
 
 class Index extends React.Component {
   render() {
-    return <Row className="main" style={{ background: `linear-gradient(45deg, ${'#A83279, #FFB246'})` }} />;
+    return (
+      <div>
+        <Row className="main" style={{ background: `linear-gradient(45deg, ${'#A83279, #FFB246'})` }}>
+          <ReactSVG src={indexSVG} className="separator" />
+        </Row>
+      </div>
+    );
   }
 }
 
