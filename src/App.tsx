949771eed33ce76735bb9b14869a56bdf030caba
Furkan Bozdag <bozd4g@hotmail.com>
2019-01-24 23:00:35 +0300

    Add Logo component. Edit ui file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Index from './scenes/Index';
+import Logo from './components/Logo';
 
 class App extends React.Component {
   public render() {
     return (
       <div style={{ height: '100vh', background: '#f5f5f5' }}>
+        <Logo />
         <Index />
       </div>
     );
