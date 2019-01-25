70ec6f8c0c1a0622ec8ca4eb1ec6e44c58616ee4
Furkan Bozdag <bozd4g@hotmail.com>
2019-01-25 23:33:59 +0300

    Add introduction side

--- src/scenes/Index/index.tsx
+++ src/scenes/Index/index.tsx
@@ -9,6 +9,39 @@ class Index extends React.Component {
     return (
       <div>
         <Row className="main" style={{ background: `linear-gradient(45deg, ${'#A83279, #FFB246'})` }}>
+          <div className="about">
+            <h1 className="bigTitle">it's me.</h1>
+            <div className="information">
+              <h2 className="first">hey, keep calm!</h2>
+              <h2 className="second">It's me.</h2>
+              <p>As you said, I'm Furkan.</p>
+              <p>
+                If I have to introduce myself; I’m designing as hobby, coding as lovely and sometimes writing something at somewhere like
+                <b>
+                  <a href="#" rel="noopener noreferrer" target="_blank">
+                    {' '}
+                    twitter
+                  </a>
+                </b>
+                ,
+                <b>
+                  <a href="#" rel="noopener noreferrer" target="_blank">
+                    {' '}
+                    medium
+                  </a>
+                </b>{' '}
+                and
+                <b>
+                  <a href="#" rel="noopener noreferrer" target="_blank">
+                    {' '}
+                    github
+                  </a>
+                </b>
+                .
+              </p>
+            </div>
+          </div>
+
           <ReactSVG src={indexSVG} className="separator" />
         </Row>
 
