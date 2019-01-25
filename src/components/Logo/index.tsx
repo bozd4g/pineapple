26e4048f7a23f6980e89411820efa7bda7d94d24
Furkan Bozdag <bozd4g@hotmail.com>
2019-01-25 22:56:09 +0300

    Add logo and happening title

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import './index.css';
 
 class Logo extends React.Component {
   render() {
-    return <div />;
+    return (
+      <div className="title">
+        <h1 className="inline name">furkan</h1>
+        <h1 className="inline surname">bozdag</h1>
+      </div>
+    );
   }
 }
 
--- src/scenes/Index/index.tsx
+++ src/scenes/Index/index.tsx
@@ -11,6 +11,11 @@ class Index extends React.Component {
         <Row className="main" style={{ background: `linear-gradient(45deg, ${'#A83279, #FFB246'})` }}>
           <ReactSVG src={indexSVG} className="separator" />
         </Row>
+
+        <div className="happening">
+          <h1 className="inline light">what's</h1>
+          <h1 className="inline bold">happening</h1>
+        </div>
       </div>
     );
   }
