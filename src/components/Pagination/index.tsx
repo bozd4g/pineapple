bd282bd678e20cda44d2beab415537717c4fce3f
Furkan Bozdag <bozd4g@hotmail.com>
2019-02-03 18:42:11 +0300

    Edit prettier config file

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -6,37 +6,37 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 interface IPaginationProps {
-  totalSize: number;
-  eachPageSize: number;
-  currentPage: number;
+    totalSize: number;
+    eachPageSize: number;
+    currentPage: number;
 }
 class Pagination extends React.Component<IPaginationProps> {
-  constructor(props: IPaginationProps) {
-    super(props);
-  }
+    constructor(props: IPaginationProps) {
+        super(props);
+    }
 
-  render() {
-    const p = this.props;
-    const totalPageSize = p.totalSize / p.eachPageSize;
+    render() {
+        const p = this.props;
+        const totalPageSize = p.totalSize / p.eachPageSize;
 
-    return (
-      <Row className="pagination">
-        <Row className="items">
-          <IconButton className="arrow" disabled={p.currentPage === 1}>
-            <FontAwesomeIcon icon={faChevronLeft} fixedWidth />
-          </IconButton>
+        return (
+            <Row className="pagination">
+                <Row className="items">
+                    <IconButton className="arrow" disabled={p.currentPage === 1}>
+                        <FontAwesomeIcon icon={faChevronLeft} fixedWidth />
+                    </IconButton>
 
-          <Fab className="active">1</Fab>
-          <Fab>2</Fab>
-          <Fab>3</Fab>
+                    <Fab className="active">1</Fab>
+                    <Fab>2</Fab>
+                    <Fab>3</Fab>
 
-          <IconButton className="arrow" disabled={totalPageSize - 1 === p.currentPage}>
-            <FontAwesomeIcon icon={faChevronRight} fixedWidth />
-          </IconButton>
-        </Row>
-      </Row>
-    );
-  }
+                    <IconButton className="arrow" disabled={totalPageSize - 1 === p.currentPage}>
+                        <FontAwesomeIcon icon={faChevronRight} fixedWidth />
+                    </IconButton>
+                </Row>
+            </Row>
+        );
+    }
 }
 
 export default Pagination;
