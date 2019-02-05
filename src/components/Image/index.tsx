acf943d828fe169d9fa77d9152e545c8967a5fba
Furkan Bozdag <bozd4g@hotmail.com>
2019-02-05 21:46:53 +0300

    Add lazy image loading package

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -2,22 +2,22 @@ import React from 'react';
 import './index.css';
 
 interface IImageProps {
-  src?: string;
-  width?: string | number;
+   src?: string;
+   width?: string | number;
 }
 
 class Image extends React.Component<IImageProps> {
-  constructor(props: IImageProps) {
-    super(props);
-  }
+   constructor(props: IImageProps) {
+      super(props);
+   }
 
-  render() {
-    return (
-      <div className="image">
-        <img src={this.props.src} style={{ width: this.props.width }} />
-      </div>
-    );
-  }
+   render() {
+      return (
+         <div className="image">
+            <img src={this.props.src} style={{ width: this.props.width }} />
+         </div>
+      );
+   }
 }
 
 export default Image;
