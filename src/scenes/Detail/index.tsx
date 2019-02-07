4927bb03e3276d735757b2217656f3aa77162fc1
Furkan Bozdag <bozd4g@hotmail.com>
2019-02-07 22:16:27 +0300

    Complete image side of Detail

--- src/scenes/Detail/index.tsx
+++ src/scenes/Detail/index.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 import Footer from '../../components/Footer';
 import Logo from '../../components/Logo';
 import './index.css';
-import Image from '../../components/Image';
+import { Row } from 'react-flexbox-grid';
 
 class Detail extends React.Component {
    render() {
       return (
          <div style={{ backgroundColor: '#f5f5f5' }}>
             <Logo />
-           <div>
-           <Image src="https://cdn-images-1.medium.com/max/1000/1*_2ynzQ0p5oCxcYOU48PujQ.jpeg" width="100%" style={{ maxHeight: '70vh', overflow: 'hidden' }} />
-           </div>
+            <div className="cover">
+               <img src="https://cdn-images-1.medium.com/max/1000/1*_2ynzQ0p5oCxcYOU48PujQ.jpeg" />
+            </div>
 
+            <Row>asd</Row>
             <Footer type="secondary" />
          </div>
       );
