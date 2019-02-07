d2a5634abe3346d2eefaafa929a9fca2c361ddce
Furkan Bozdag <bozd4g@hotmail.com>
2019-02-07 22:33:44 +0300

    Text side of Detail has been created

--- src/scenes/Detail/index.tsx
+++ src/scenes/Detail/index.tsx
@@ -2,18 +2,24 @@ import React from 'react';
 import Footer from '../../components/Footer';
 import Logo from '../../components/Logo';
 import './index.css';
-import { Row } from 'react-flexbox-grid';
+import { Row, Col } from 'react-flexbox-grid';
 
 class Detail extends React.Component {
    render() {
       return (
          <div style={{ backgroundColor: '#f5f5f5' }}>
             <Logo />
-            <div className="cover">
-               <img src="https://cdn-images-1.medium.com/max/1000/1*_2ynzQ0p5oCxcYOU48PujQ.jpeg" />
+            <div className="detail">
+               <div className="cover">
+                  <img src="https://cdn-images-1.medium.com/max/1000/1*_2ynzQ0p5oCxcYOU48PujQ.jpeg" />
+               </div>
+               <Row center="xs" className="post-detail">
+                  <Col xs={10} className="wrapper">
+                     asd
+                  </Col>
+               </Row>
             </div>
 
-            <Row>asd</Row>
             <Footer type="secondary" />
          </div>
       );
